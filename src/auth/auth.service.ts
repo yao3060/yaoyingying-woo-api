1eb660086d42daa1164aaa38ca43550e8409e8bb
YAO YINGYING <yao3060@gmail.com>
2022-10-14 03:00:12 +0800

    add create user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,41 @@
-import { Injectable } from '@nestjs/common';
-import { User, Bookmark } from '@prisma/client';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon2 from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable()
 export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
   signin() {
     return {
       message: 'signin',
     };
   }
 
-  signup() {
-    //
+  async signup(dto: AuthDto) {
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          password: await argon2.hash(dto.password),
+        },
+      });
+
+      delete user.password;
+      return {
+        code: 200,
+        message: 'User Created',
+        data: user,
+      };
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        throw new ForbiddenException({
+          code: 403,
+          message: error.message,
+        });
+      }
+    }
   }
 }
